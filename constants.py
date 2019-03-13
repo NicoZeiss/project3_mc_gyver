354d203ac9e391013202d5a449fc0481a24b6947
NicoZeiss <zeissern@gmail.com>
2019-03-13 16:54:19 +0100

    constants.py filled with window size, sprites size and a list of the pictures used

---
+++ constants.py
@@ -0,0 +1,15 @@
+"""Here are the constants we'll use in our game"""
+
+#Window's constants
+sprites_per_side = 15
+sprite_size = 45
+window_size = sprites_per_side * sprite_size
+
+window_title = "MacGyver Maze"
+image_icone = "pictures/mcgyver.png"
+
+#List of pictures we'll use in our game
+background_pict = "pictures/background.png"
+mcgyver_pict = "pictures/mcgyver.png"
+murdoc_pict = "pictures/murdoc.png"
+wall_pict = "pictures/wall.jpg"
